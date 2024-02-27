2caa1ef1b9e5fa0350383e112b58eaba8ebcfc58
Özgürhan Polat <ozgurhan.45@gmail.com>
2024-02-27 17:34:33 +0300

    some ui components are added

---
+++ src/main/java/org/ozgur/ui/concretes/GetAllTodosByPriorityComponent.java
@@ -0,0 +1,10 @@
+package org.ozgur.ui.concretes;
+
+import org.ozgur.ui.abstracts.Component;
+
+public class GetAllTodosByPriorityComponent implements Component {
+    @Override
+    public void showInterface() {
+
+    }
+}
---
+++ src/main/java/org/ozgur/ui/concretes/SendNotificationComponent.java
@@ -0,0 +1,10 @@
+package org.ozgur.ui.concretes;
+
+import org.ozgur.ui.abstracts.Component;
+
+public class SendNotificationComponent implements Component {
+    @Override
+    public void showInterface() {
+
+    }
+}
